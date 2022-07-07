1aeca813c9b2b6b40a28e720334420aeb2aaaf22
slayerbrk <Bradley.kars@gmail.com>
2022-07-06 21:22:43 -0400

    Create IT244_Kars_Unit8.py
    IT244 Unit 8 Assignment: Working With Python System Interfaces

---
+++ IT244_Kars_Unit8.py
@@ -0,0 +1,31 @@
+import subprocess
+import sys
+import os
+
+# Part 1:
+
+#user = os.getlogin()
+#print(user)
+#exec_path = os.get_exec_path()
+#print(exec_path)
+
+# Part 2:
+
+#sys_path = sys.path
+#print(sys_path)
+#sys_byteorder = sys.byteorder
+#print(sys_byteorder)
+
+# Part 3:
+
+#listdir = os.listdir(path='c:\\')
+#print(listdir)
+#os.mkdir ('c:\\tempPython')
+#listdir = os.listdir(path='c:\\')
+#print(listdir)
+#os.rmdir ('c:\\tempPython')
+
+# Part 4:
+
+#subprocess.Popen ('C:\\windows\\system32\\cmd.exe "/c dir C:\\ >> C:\\pythonOut.txt')
+#subprocess.Popen ('calc.exe')
