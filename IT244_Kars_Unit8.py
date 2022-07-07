6bd19eb0aa84be3f2e936d42d049cd9600fabde8
slayerbrk <Bradley.kars@gmail.com>
2022-07-06 21:24:22 -0400

    Update IT244_Kars_Unit8.py

--- IT244_Kars_Unit8.py
+++ IT244_Kars_Unit8.py
@@ -4,28 +4,28 @@ import os
 
 # Part 1:
 
-#user = os.getlogin()
-#print(user)
-#exec_path = os.get_exec_path()
-#print(exec_path)
+user = os.getlogin()
+print(user)
+exec_path = os.get_exec_path()
+print(exec_path)
 
 # Part 2:
 
-#sys_path = sys.path
-#print(sys_path)
-#sys_byteorder = sys.byteorder
-#print(sys_byteorder)
+sys_path = sys.path
+print(sys_path)
+sys_byteorder = sys.byteorder
+print(sys_byteorder)
 
 # Part 3:
 
-#listdir = os.listdir(path='c:\\')
-#print(listdir)
-#os.mkdir ('c:\\tempPython')
-#listdir = os.listdir(path='c:\\')
-#print(listdir)
-#os.rmdir ('c:\\tempPython')
+listdir = os.listdir(path='c:\\')
+print(listdir)
+os.mkdir ('c:\\tempPython')
+listdir = os.listdir(path='c:\\')
+print(listdir)
+os.rmdir ('c:\\tempPython')
 
 # Part 4:
 
-#subprocess.Popen ('C:\\windows\\system32\\cmd.exe "/c dir C:\\ >> C:\\pythonOut.txt')
-#subprocess.Popen ('calc.exe')
+subprocess.Popen ('C:\\windows\\system32\\cmd.exe "/c dir C:\\ >> C:\\pythonOut.txt')
+subprocess.Popen ('calc.exe')
